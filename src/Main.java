8a09b60f1244683ab7d50767c9c060918762fc66
ambriel2016 <crb3305@gmail.com>
2020-01-22 15:52:00 -0500

    An application that assigns two numbers to two variables

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        // declare two different variables
+        int myNumber = 2;
+        float namedMyOtherNumber = 1.7938f;
+        // printed out the two different variables
+        System.out.println("My numbers are " + myNumber + " and " + namedMyOtherNumber + ".");
+    }
+}
